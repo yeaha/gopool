db1aebb864c0c332731181fed1949b1068f58957
杨益 <yangyi.cn.gz@gmail.com>
2019-06-25 15:55:36 +0800

    benchmark functions

--- pool_test.go
+++ pool_test.go
@@ -2,7 +2,9 @@ package gopool
 
 import (
 	"context"
+	"fmt"
 	"math/rand"
+	"sync"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -86,13 +88,78 @@ func TestPool_DoError(t *testing.T) {
 	})
 }
 
-func BenchmarkPool(b *testing.B) {
-	p := New(100, 5*time.Second)
-	for i := 0; i < b.N; i++ {
-		p.Do(context.Background(), func() {
-			n := rand.Int63n(50)
-			time.Sleep(time.Duration(n) * time.Microsecond)
+func BenchmarkMain(b *testing.B) {
+	idle := 5 * time.Second
+
+	testTask := func() {
+		n := rand.Int63n(50)
+		time.Sleep(time.Duration(n) * time.Microsecond)
+	}
+
+	for _, workers := range []int{10, 50, 100, 500, 1000} {
+		b.Run(fmt.Sprintf("worker %d", workers), func(b *testing.B) {
+			b.Run("pool", func(b *testing.B) {
+				p := New(workers, idle)
+
+				b.StartTimer()
+				for i := 0; i < b.N; i++ {
+					p.Do(context.Background(), testTask)
+				}
+				p.Close()
+				b.StopTimer()
+			})
+
+			b.Run("fan out", func(b *testing.B) {
+				var wg sync.WaitGroup
+				tasks := make(chan func())
+
+				for i := 0; i < workers; i++ {
+					wg.Add(1)
+					go func() {
+						defer wg.Done()
+
+						for task := range tasks {
+							task()
+						}
+					}()
+				}
+
+				b.StartTimer()
+				for i := 0; i < b.N; i++ {
+					tasks <- testTask
+				}
+				close(tasks)
+				wg.Wait()
+				b.StopTimer()
+			})
+
+			b.Run("token bucket", func(b *testing.B) {
+				tokens := make(chan struct{}, workers)
+				tasks := make(chan task)
+
+				var wg sync.WaitGroup
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+
+					for task := range tasks {
+						tokens <- struct{}{}
+
+						go func(task func()) {
+							task()
+							<-tokens
+						}(task)
+					}
+				}()
+
+				b.StartTimer()
+				for i := 0; i < b.N; i++ {
+					tasks <- testTask
+				}
+				close(tasks)
+				wg.Wait()
+				b.StopTimer()
+			})
 		})
 	}
-	p.Close()
 }
